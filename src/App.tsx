a3364557d73a38371fa486314281e7e5f35d85df
FeehOliveira <feeholiveira10@outlook.com>
2024-07-24 21:00:40 -0300

    estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
    <div>
     <h1>GitHub Finder</h1>
+    <Outlet />
    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./routes/Home"/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
